be4243e01c1961de99d05b902ae82735f267fe7c
prajwalthaware <prajwalthaware@gmail.com>
2025-05-20 04:46:40 +0530

    Delete Terraform_ directory

--- Terraform_/Terraform_/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "deployment_status" {
-  description = "Deployment completion status"
-  value       = "Infrastructure deployment completed successfully"
-}
-
-output "application_access" {
-  description = "Access information for the deployed application"
-  value       = "Your application is available at: http://${module.compute.instance_public_ip}"
-}
